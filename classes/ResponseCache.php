a97de16a54e9e5dae2d85dff57483826f8d40e73
Abhi Beckert <abhi@abhibeckert.com>
2015-10-25 09:44:11 +1000

    Completely disable response cache

--- classes/ResponseCache.php
+++ classes/ResponseCache.php
@@ -18,68 +18,68 @@ class ResponseCache
   
   static public function start()
   {
-    if ($_SERVER['REQUEST_METHOD'] != 'GET') {
-      return;
-    }
-    
-    $cacheSeed = sha1(serialize([
-      Env::get('request_url')
-    ]));
-    
-    ob_start();
-    self::$willCache = true;
+//     if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+//       return;
+//     }
+//     
+//     $cacheSeed = sha1(serialize([
+//       Env::get('request_url')
+//     ]));
+//     
+//     ob_start();
+//     self::$willCache = true;
   }
   
   static public function saveCacheIfEnabled()
   {
-    global $db;
-    
-    if (!self::$willCache) {
-      return;
-    }
-    
-    $url = Env::get('request_url');
-    if ($url == '/') {
-      $url = '/index';
-    }
-    
-    $cacheFile = Env::get('base_path') . ltrim($url, '/') . '.html';
-    
-    $contents = ob_get_flush();
-    
-    if (!is_dir(dirname($cacheFile))) {
-      mkdir(dirname($cacheFile), 0777, true);
-    }
-    
-    file_put_contents($cacheFile, $contents);
-    
-    $db->cached_pages->write(sha1($url), (object)[
-      'expires' => strtotime('30 days'),
-      'url' => $url
-    ], true);
+//     global $db;
+//     
+//     if (!self::$willCache) {
+//       return;
+//     }
+//     
+//     $url = Env::get('request_url');
+//     if ($url == '/') {
+//       $url = '/index';
+//     }
+//     
+//     $cacheFile = Env::get('base_path') . ltrim($url, '/') . '.html';
+//     
+//     $contents = ob_get_flush();
+//     
+//     if (!is_dir(dirname($cacheFile))) {
+//       mkdir(dirname($cacheFile), 0777, true);
+//     }
+//     
+//     file_put_contents($cacheFile, $contents);
+//     
+//     $db->cached_pages->write(sha1($url), (object)[
+//       'expires' => strtotime('30 days'),
+//       'url' => $url
+//     ], true);
   }
   
   static public function disable()
   {
-    if (self::$willCache) {
-      ob_end_flush();
-    }
-    self::$willCache = false;
+//     if (self::$willCache) {
+//       ob_end_flush();
+//     }
+//     self::$willCache = false;
   }
   
   static public function flushAll()
   {
-    global $db;
-    
-    $entries = $db->cached_pages->find();
-    foreach ($entries as $id => $row) {
-      $cacheFile = Env::get('base_path') . ltrim($row->url, '/') . '.html';
-      if (file_exists($cacheFile)) {
-        unlink($cacheFile);
-      }
-      
-      $db->cached_pages->purge($id);
-    }
-    
+//     global $db;
+//     
+//     $entries = $db->cached_pages->find();
+//     foreach ($entries as $id => $row) {
+//       $cacheFile = Env::get('base_path') . ltrim($row->url, '/') . '.html';
+//       if (file_exists($cacheFile)) {
+//         unlink($cacheFile);
+//       }
+//       
+//       $db->cached_pages->purge($id);
+//     }
+//     
   }
 }
