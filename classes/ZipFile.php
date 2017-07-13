5f62b7f6aa16931bcafb42b8867f2c7d7fe9b005
Abhi Beckert <abhi@abhibeckert.com>
2017-07-13 10:48:20 +1000

    Improvements to ZipFile

--- classes/ZipFile.php
+++ classes/ZipFile.php
@@ -9,22 +9,33 @@ namespace Fierce;
 
 class ZipFile
 {
-  public $path;
+  public $path = null;
+  public $tmpDir = null; // by default this will be /tmp/<rand>. You can provide a different path.
   
   public function __construct($path)
   {
-    if ($path[0] != '/') {
+    if ($path != null && $path[0] != '/') {
       throw new \Exception('must be an absolute path');
     }
-    $this->path = $path;
+    
+    if ($path) {
+      $this->path = $path;
+      
+      if (file_exists($this->path)) {
+        throw new \Exception('Path already exists');
+      }
+      
+      file_put_contents($this->path, '');
+      
+      if (!file_exists($this->path)) {
+        throw new \Exception('Unable to create zip');
+      }
+    }
   }
   
   public function addFromString($path, $contents)
   {
-    // create a tmp dir
     $tmp = self::createTmpDir();
-    $oldCwd = getcwd();
-    chdir($tmp);
     
     // create parent dir if needed
     $parentDir = dirname("$tmp/$path");
@@ -40,19 +51,6 @@ class ZipFile
       print($bytesWritten);
       throw new \Exception("error adding to zip");
     }
-    
-    // add tmp file to the zip
-    $zipPath = escapeshellarg($this->path);
-    $filePath = escapeshellarg($path);
-    $error = shell_exec("/usr/bin/zip -q $zipPath $filePath 2>&1");
-    
-    if ($error != '') {
-      throw new \Exception("error adding to zip: $error");
-    }
-    
-    // clean up
-    shell_exec('rm -r ' . escapeshellarg($tmp));
-    chdir($oldCwd);
   }
   
   public function addFile($sourceFile, $path)
@@ -63,9 +61,7 @@ class ZipFile
     
     // create a tmp dir, and write contents to it
     $tmp = self::createTmpDir();
-    $oldCwd = getcwd();
-    chdir($tmp);
-    
+
     // create parent dir if needed
     $parentDir = dirname("$tmp/$path");
     
@@ -79,23 +75,44 @@ class ZipFile
     if (!$success) {
       throw new \Exception("error adding to zip");
     }
+  }
+  
+  public function complete($compressionLevel='-6')
+  {
+    if (!$this->path) {
+      throw new \Exception('stream not yet implemented.'); // see https://stackoverflow.com/questions/4357073/lamp-how-to-create-zip-of-large-files-for-the-user-on-the-fly-without-disk-cp
+    }
     
-    // add tmp file to the zip
+    
+    if (file_exists($this->path)) {
+      if (filesize($this->path) != 0) {
+        throw new \Exception('Path already exists');
+      } else {
+        unlink($this->path);
+      }
+    }
+    
+    $tmp = self::createTmpDir();
+    $oldCwd = getcwd();
+    chdir($tmp);
+    
     $zipPath = escapeshellarg($this->path);
-    $filePath = escapeshellarg($path);
-    $error = shell_exec("/usr/bin/zip -q $zipPath $filePath 2>&1");
+    $error = shell_exec("/usr/bin/find . | /usr/bin/zip $compressionLevel -q -@ $zipPath 2>&1");
     
     if ($error != '') {
       throw new \Exception("error adding to zip: $error");
     }
-    
     // clean up
-    shell_exec('rm -r ' . escapeshellarg($tmp));
     chdir($oldCwd);
+    shell_exec('rm -r ' . escapeshellarg($tmp));
   }
   
   private function createTmpDir()
   {
+    if ($this->tmpDir) {
+      return $this->tmpDir;
+    }
+    
     $systemTmp = sys_get_temp_dir();
     rtrim($systemTmp, '/');
     
@@ -109,6 +126,8 @@ class ZipFile
         throw new \Exception('cannot create tmp dir');
       }
     }
+    
+    $this->tmpDir = $tmpDir;
     
     return $tmpDir;
   }
